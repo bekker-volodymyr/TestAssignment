1ac4b8168ae72d8a3ab895708167dad36afd0319
YurkevichVA <bekker.volodymyr.yu@gmail.com>
2024-10-16 15:39:11 +0300

    rotate turret on touch began

--- Assets/Scripts/Turret/TurretRotate.cs
+++ Assets/Scripts/Turret/TurretRotate.cs
@@ -12,6 +12,20 @@ namespace TestAssignment.Turret
             {
                 Touch touch = Input.GetTouch(0);
 
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        Vector3 targetPosition = hit.point;
+                        Vector3 direction = new Vector3(targetPosition.x, transform.position.y, targetPosition.z) - transform.position;
+                        Quaternion targetRotation = Quaternion.LookRotation(direction);
+                        transform.rotation = targetRotation;
+                    }
+                }
+
                 if (touch.phase == TouchPhase.Moved)
                 {
                     float horizontalMovement = touch.deltaPosition.x;
